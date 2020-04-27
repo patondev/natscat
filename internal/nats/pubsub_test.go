cbf67e3a71b83af376ed8185dcbfa144e3588fa6
Habibie Faried <habibiefaried@gmail.com>
2020-04-27 05:20:58 +0000

    Adding unit test

--- internal/nats/pubsub_test.go
+++ internal/nats/pubsub_test.go
@@ -5,8 +5,9 @@ import (
     "testing"
     "math/rand"
     "time"
-    //"github.com/nats-io/nats.go"
+    "github.com/nats-io/nats.go"
     "syscall"
+    "strings"
 )
 
 var natsURL = "nats://127.0.0.1:4222" // you can change this
@@ -22,6 +23,7 @@ func randString(length int) string {
 }
 
 func TestPubSub(t *testing.T) {
+	// To make sure it receives the message
 	psubject := "subject_"+randString(5)
 	mpub := "message_"+randString(10)
 
@@ -32,7 +34,7 @@ func TestPubSub(t *testing.T) {
 		npub.Publish()
 		time.Sleep(2 * time.Second)
 		log.Printf("Subject %v with Message %v created!\n",psubject, mpub)
-		npub := NatsClass{DefaultURL: natsURL, PubSubject: psubject, Message: mpub}
+		npub = NatsClass{DefaultURL: natsURL, PubSubject: psubject, Message: mpub}
 		npub.Publish()
 		time.Sleep(2 * time.Second)
 		syscall.Kill(syscall.Getpid(), syscall.SIGHUP) // kill that
@@ -40,4 +42,31 @@ func TestPubSub(t *testing.T) {
 
 	nsub := NatsClass{DefaultURL: natsURL, SubsSubject: psubject}
 	nsub.SubscribeListen()
+}
+
+func TestPubWithVerification(t *testing.T) {
+	// To make sure that the message is the same as when it's being sent
+	psubject := "subject_"+randString(15)
+	mpub := "message_"+randString(25)
+
+	go func(){
+		time.Sleep(2 * time.Second)
+		log.Printf("Subject %v with Message %v created!\n",psubject, mpub)
+		npub := NatsClass{DefaultURL: natsURL, PubSubject: psubject, Message: mpub}
+		npub.Publish()
+	}()
+
+	nc, _ := nats.Connect(natsURL)
+	defer nc.Close()
+
+	sub, _ := nc.SubscribeSync(psubject)
+	m, err := sub.NextMsg(3 * time.Second)
+	if err == nil {
+		recvmsg := string(m.Data)
+		if (strings.Compare(recvmsg,mpub) != 0){
+			t.Errorf("The string received is not the same, should be %v but got %v",mpub,recvmsg)
+		}
+	} else {
+	    t.Errorf("NextMsg timed out.")
+	}
 }
